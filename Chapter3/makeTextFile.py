fa4784ee39fd0665cba0f9fd958b880f799059ba
浮生若梦 <microease@163.com>
2019-01-21 17:35:55 +0800

    add

--- Chapter3/makeTextFile.py
+++ Chapter3/makeTextFile.py
@@ -7,3 +7,18 @@ while True:
         print("Error: %s already exists" % fname)
     else:
         break
+
+all = []
+
+print("\nEnter lines ('.' by itself to quit).\n")
+# loop until user terminates input
+while True:
+    entry = input('> ')
+    if entry == '.':
+        break
+    else:
+        all.append(entry)
+fobj = open(fname, 'w')
+fobj.writelines(['%s%s' % (x, ls) for x in all])
+fobj.close()
+print("Done!")
